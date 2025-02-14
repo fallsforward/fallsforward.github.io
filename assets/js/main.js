273784479104e82255066a98d3846720d8f0a22a
Edward Brey <edward@breyfamily.net>
2025-02-14 15:59:04 -0600

    Lead with issues.

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,19 +10,6 @@
   "use strict";
 
   /**
-   * Apply .scrolled class to the body as the page is scrolled down
-   */
-  function toggleScrolled() {
-    const selectBody = document.querySelector('body');
-    const selectHeader = document.querySelector('#header');
-    if (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top')) return;
-    window.scrollY > 40 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
-  }
-
-  document.addEventListener('scroll', toggleScrolled);
-  window.addEventListener('load', toggleScrolled);
-
-  /**
    * Mobile nav toggle
    */
   const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
@@ -124,7 +111,7 @@
         slidesPerView: 1,
         spaceBetween: 20
       },
-      1200: {
+      992: {
         slidesPerView: 3,
         spaceBetween: 20
       }
